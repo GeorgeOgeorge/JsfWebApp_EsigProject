957b9d4a2c9b09f4e2808de4484e79313653b77c
George Mendonça <61426720+GeorgeOgeorge@users.noreply.github.com>
2022-01-08 20:47:59 -0300

    Added ArchUnit testing

--- src/test/java/arc/Models.java
+++ src/test/java/arc/Models.java
@@ -4,8 +4,6 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
